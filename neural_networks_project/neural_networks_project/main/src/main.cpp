eaf126cd6419a96b90542042fb5c7651c8182bb7
Matej Gomboc <matej@irnas.eu>
2016-10-31 11:26:19 +0100

    Test simulation update

--- neural_networks_project/neural_networks_project/main/src/main.cpp
+++ neural_networks_project/neural_networks_project/main/src/main.cpp
@@ -1,11 +1,18 @@
 #include "stdafx.h"
 #include <iostream>
+
+#include "debug_network.h"
 #include "simulation.h"
 
 using namespace std;
+using namespace neural_networks;
 
 int main(int argc, char* argv[])
 {
-	test_genetic_algorythm();
+	while(true)
+	{
+		debug::debug_network();
+		test_genetic_algorythm();
+	}
 	return 0;
 }
